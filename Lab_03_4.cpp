a83bc9260ffce33c6ed5e8edc18d972918d04956
sozanska <116510991+sozanska@users.noreply.github.com>
2022-11-09 14:49:38 +0200

    Add files via upload

---
+++ Lab_03_4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;  // вхідний аргумент
+	double y;  // вхідний аргумент
+	double R;  // вхідний параметр
+
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	// розгалуження в повній формі
+	if (((x * x + y * y) <= pow(R, 2) && y >= 0) || (y >= -R && x >= y && x <= 0))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+
+	cin.get();
+	return 0;
+}
